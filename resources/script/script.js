932d437e22346454160db2024bc7f856c6b4f917
Ian Valdovinos <ian.valdovinos752001@gmail.com>
2020-09-08 19:43:30 -0400

    Add home page and color change function in script.js

--- resources/script/script.js
+++ resources/script/script.js
@@ -2,10 +2,25 @@ const buttonAboutMe = document.getElementById('about-me');
 const buttonPortfolio = document.getElementById('portfolio');
 const buttonExperience = document.getElementById('experience');
 
+const rightSection = document.getElementById('right-section');
 const aboutMePage = document.getElementById('about-me-page');
 const portfolioPage = document.getElementById('portfolio-page');
 const experiencePage = document.getElementById('experience-page');
 const homePage = document.getElementById('home-page');
+
+// Page colors
+const homePageColor = '#bbe1fa';
+const homePageBorderColor = '#3282b8';
+
+
+// Functionality
+const changePageColors = (back, border) => {
+    rightSection.style.backgroundColor = back;
+    rightSection.style.border = `3px solid ${border}`;
+};
+
+changePageColors(homePageColor, homePageBorderColor);
+
 
 function navButtonClick(event) {
 
